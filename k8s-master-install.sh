61a61ed3ac414940028ff2c9468e1d4a11a644f1
satyadhenuvakonda <satya.dhenuvakonda@gmail.com>
2020-04-12 13:51:09 +0100

    Adding Flannel

--- k8s-master-install.sh
+++ k8s-master-install.sh
@@ -43,5 +43,28 @@ kube-join = sudo kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 kubectl version
+
+# Join the two Kube worker nodes to the cluster
+
+#    Copy the kubeadm join command that was printed by the kubeadm init command earlier, with the token and hash. Run this command on both worker nodes, but make sure you add sudo in front of it:
+####################################################################################################
+#    sudo kubeadm join $some_ip:6443 --token $some_token --discovery-token-ca-cert-hash $some_hash
+####################################################################################################
+#    Now, on the Kube master node, make sure your nodes joined the cluster successfully:
+#    kubectl get nodes
+#     Verify that all three of your nodes are listed. It will look something like this:
+#      NAME            STATUS     ROLES    AGE   VERSION
+#      ip-10-0-1-101   NotReady   master   30s   v1.12.2
+#      ip-10-0-1-102   NotReady   <none>   8s    v1.12.2
+#      ip-10-0-1-103   NotReady   <none>   5s    v1.12.2
+#     Note that the nodes are expected to be in the NotReady state for now.
+
+####################################################################################################
+# Set up cluster networking with flannel
+#    Turn on iptables bridge calls on all three nodes:
+####################################################################################################
+echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+#    Next, run this only on the Kube master node:
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
