3e22800d7c32988707525a7276dcd2db892cd6b4
satyadhenuvakonda <satya.dhenuvakonda@gmail.com>
2020-04-12 14:24:47 +0100

    Updated with flannel

--- k8s-master-install.sh
+++ k8s-master-install.sh
@@ -66,5 +66,7 @@ kubectl version
 ####################################################################################################
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
+
+
 #    Next, run this only on the Kube master node:
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
