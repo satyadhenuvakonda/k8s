10c72a762f86d03b8a955dc622f7aa6cb4cdfc35
satyadhenuvakonda <satya.dhenuvakonda@gmail.com>
2020-04-14 14:02:58 +0100

    Updated and worked

--- k8s-master-install.sh
+++ k8s-master-install.sh
@@ -2,18 +2,16 @@
 
 # Add the Docker GPG key:
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
 # Add the Docker repository:
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
-
 # This update will add the localrepo
 sudo apt-get update
-# Installing docker docker-ce=18
+
+# Installing docker docker-ce=18 and hold a specific version
 sudo apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
-# Hold Docker at this specific version:
 sudo apt-mark hold docker-ce
 
 # check the status of docker
@@ -24,9 +22,8 @@ else
     echo FAILED-TO-START-DOCKER
 fi
 
-# Add the Kubernetes GPG key:
- curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-# Add the Kubernetes repository:
+# Add the Kubernetes GPG key: && # Add the Kubernetes repository:
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
@@ -45,28 +42,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl version
 
-# Join the two Kube worker nodes to the cluster
-
-#    Copy the kubeadm join command that was printed by the kubeadm init command earlier, with the token and hash. Run this command on both worker nodes, but make sure you add sudo in front of it:
-####################################################################################################
-#    sudo kubeadm join $some_ip:6443 --token $some_token --discovery-token-ca-cert-hash $some_hash
-####################################################################################################
-#    Now, on the Kube master node, make sure your nodes joined the cluster successfully:
-#    kubectl get nodes
-#     Verify that all three of your nodes are listed. It will look something like this:
-#      NAME            STATUS     ROLES    AGE   VERSION
-#      ip-10-0-1-101   NotReady   master   30s   v1.12.2
-#      ip-10-0-1-102   NotReady   <none>   8s    v1.12.2
-#      ip-10-0-1-103   NotReady   <none>   5s    v1.12.2
-#     Note that the nodes are expected to be in the NotReady state for now.
-
-####################################################################################################
-# Set up cluster networking with flannel
-#    Turn on iptables bridge calls on all three nodes:
-####################################################################################################
 echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
-
 
 #    Next, run this only on the Kube master node:
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
